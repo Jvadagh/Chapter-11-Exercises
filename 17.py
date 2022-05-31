b379de373f3992e25cc7796eabc568ff3c123f86
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-05-31 01:34:34 -0700

    initial commit

---
+++ 17.py
@@ -0,0 +1,3 @@
+"""
+17. Are the elements in dictionaries ordered or unordered?
+"""
---
+++ 18.py
@@ -0,0 +1,3 @@
+"""
+18. Modify Listing 9.14 (tkinterlight.py) so that it models a light with a single on/off yellow lamp.
+"""
