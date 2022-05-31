d5dbcaaf8b5668675eeff282d75f3661f39977f2
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-05-31 00:57:20 -0700

    initial commit

---
+++ 12.py
@@ -0,0 +1,7 @@
+"""
+if d refers to a dictionary, what expression represents the value associated with the key "Fred"?
+there is 2 way to do this ==> look at this example please:
+"""
+d = {"Fred": 19}
+print(d.get("Fred"))
+print(d["Fred"])
