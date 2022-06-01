7fb3bd295c385387b2788cf01b51d7e48911d157
V3il3DOraCl3 <javadaaghajanloo@gmail.com>
2022-06-01 00:36:49 -0700

    initial commit

--- 18.py
+++ 18.py
@@ -1,3 +1,31 @@
 """
 18. Modify Listing 9.14 (tkinterlight.py) so that it models a light with a single on/off yellow lamp.
 """
+
+from tkinter import Tk, Canvas
+from tkinter.ttk import Button, Frame
+
+
+def DoOnOff():
+    global color
+    if color == 'yellow':
+        color = 'black'
+        canvas.itemconfigure(yellow_lamp, fill='black')
+
+    elif color == 'black':
+        color = 'yellow'
+        canvas.itemconfigure(yellow_lamp, fill='yellow')
+
+
+color = 'yellow'
+root = Tk()
+root.title("yellow light")
+frame = Frame(root)
+frame.pack()
+canvas = Canvas(frame, width=300, height=300)
+canvas.create_rectangle(50, 20, 150, 280, fill='gray')
+yellow_lamp = canvas.create_oval(70, 120, 130, 180, fill='yellow')
+button = Button(frame, text='ON/OFF', command=DoOnOff)
+button.grid(row=0, column=0)
+canvas.grid(row=0, column=1)
+root.mainloop()
--- 19.py
+++ 19.py
@@ -1,5 +1,4 @@
 """
-19. Write a graphical, two-player Tic-Tac-Toe game using the tkinter module (see https://en.wikipedia.
-org/wiki/Tic-tac-toe for more information about the game). You can use nine separate variables
+19. Write a graphical, two-player Tic-Tac-Toe game using the tkinter module. You can use nine separate variables
 to track the contents of the game’s squares. You must be able to draw lines and circles in the appropriate locations
 """
